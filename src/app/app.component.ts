dfad2cdb1026d3955a3ffea5ba38268de3b5a4cb
Savva <savvasiry@gmail.com>
2022-06-30 04:35:03 +0300

    Add TopMenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('test', [
+      state('noActive', style({ background: 0 })),
+      state('active', style({ background: '#eeeeee' })),
+      transition('noActive <=> active', animate('0.3s')),
+    ])
+  ]
 })
+
 export class AppComponent {
-  title = 'testCLI';
+  title = 'Home';
+
+  isExpanded: boolean = false
+  state: string = 'initial'
+
+  expand() {
+    this.isExpanded = !this.isExpanded
+    this.state = this.isExpanded ? 'expanded' : 'initial'
+  }
 }
